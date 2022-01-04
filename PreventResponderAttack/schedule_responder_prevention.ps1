f20fb0bf40cbd94d36351c09d6c4a469876a177a
Eric Wojcik <eric@silocityit.com>
2022-01-04 14:03:00 -0500

    Changes to be committed: 	new file:   schedule_responder_prevention.ps1 	new file:   schedule_responder_prevention.zip

---
+++ PreventResponderAttack/schedule_responder_prevention.ps1
@@ -0,0 +1,25 @@
+﻿Set-ExecutionPolicy Unrestricted
+Get-ScheduledTask -TaskName "Prevent Responder Attack" | Unregister-ScheduledTask -Confirm:$false
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe'-Argument '-NoProfile -WindowStyle Hidden -command "& 
+{
+@echo off
+
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Wpad" /f
+REG DELETE  "HKLM\Software\policies\Microsoft\Windows NT\DNSClient" /f
+REG DELETE  "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" /f
+
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Wpad"
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Wpad" /v "WpadOverride" /t REG_DWORD /d "1" /f
+
+
+REG ADD  "HKLM\Software\policies\Microsoft\Windows NT\DNSClient"
+REG ADD  "HKLM\Software\policies\Microsoft\Windows NT\DNSClient" /v "EnableMulticast" /t REG_DWORD /d "0" /f
+REG ADD  "HKLM\Software\policies\Microsoft\Windows NT\DNSClient" /v "DisableSmartNameResolution" /t REG_DWORD /d "1" /f
+REG ADD  "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters"
+REG ADD  "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" /v "DisableParallelAandAAAA" /t REG_DWORD /d "1" /f
+}"'
+
+$trigger =  New-ScheduledTaskTrigger -AtStartup
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Prevent Responder Attack" -Description "(WPAD & LLMNR)"
+Set-ExecutionPolicy Restricted
